ab0b9b12bbb63f359dd778eafa079dc4b9d5dd45
TasbeehNaser <tasbeehnaser2002@gmail.com>
2023-10-31 15:23:09 +0200

    Modifying the structure in the Restaurant section

--- src/components/restaurant/Restaurant.jsx
+++ src/components/restaurant/Restaurant.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Pizza from './../pizza/Pizza'
+import Chickpea from './../chickpea/Chickpea'
+import Potato from './../potato/Potato'
 import './Restaurant.css'
 function Restaurant() {
   return (
     <div className='py-5 my-5'>
-    <div className='container py-5'>
-    <ul className='restaurant py-5'>
-        <li><Link className='link' to='/Pizza'>Pizza</Link></li>
-        <li><Link to='/Potato' className='link'>Potato</Link></li>
-        <li><Link to='/Chickpea' className='link'>Chickpea</Link></li>
-    </ul>
+    <div className='container'>
+   <h1 className='h1-restaurent'>My Restaurant</h1>
+      <section className='Pizza'>
+        <Pizza />
+      </section>
+      <section className='Potato'>
+        <Potato />
+      </section>
+      <section className='Chickpea'>
+        <Chickpea />
+      </section>
+    
+  
     </div>
     
    
